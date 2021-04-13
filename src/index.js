126f646a3fb5be228e05b11bd7300e435d52f74d
luikz <luiz.carlos.techie@gmail.com>
2021-04-12 22:40:40 -0300

    importação arquivo App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 reportWebVitals();
